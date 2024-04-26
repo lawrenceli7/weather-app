b4432b9d4b9b43761445f60a8cd2423d31006d26
Lawrence Li <lawrenceli3202@gmail.com>
2024-04-26 00:23:15 -0400

    Hide API key

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        WEATHER_API_KEY: process.env.API_KEY,
+    },
+};
 
 export default nextConfig;
