07e36660d418106602f7d95e57432561ca7186a2
Lawrence Li <lawrenceli3202@gmail.com>
2024-04-26 01:30:12 -0400

    Style homepage using ant design

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,39 @@
 "use client";
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Card, Divider, Input } from "antd";
 import Link from "next/link";
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 
 export default function Home() {
   const [city, setCity] = useState("");
+
+  const handleInputChange = (e: SyntheticEvent<HTMLInputElement>) => {
+    setCity((e.target as HTMLInputElement).value);
+  };
+
   return (
-    <main>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-center m-2 text-4xl text-white">Find the weather in any city!</h1>
-        <p className="text-center m-2 text-lg text-white">Enter a city name below to get the current weather.</p>
-        <input className="p-1 border border-white w-1/2 block mx-10 my-auto text-center text-black" type="text" value={city} placeholder="City name" onChange={(e) => setCity(e.target.value)} />
-        <Link className="p-1 border border-[#ddd] no-underline text-white w-1/2 block mx-0 my-auto text-center mt-5" href={`/${city}`}>Get Weather</Link>
+    <main className="h-screen flex justify-center items-center bg-blue-100">
+      <div className="flex justify-center items-center h-full">
+        <Card className="w-full h-7/24 border border-black shadow-lg">
+          <h1 className="text-2xl text-center">Find the weather in any city!</h1>
+          <Divider />
+          <div>
+            <p className="text-lg text-center">Enter a city name below to get the current weather.</p>
+            <Input
+              className="w-full mt-4"
+              placeholder="City name"
+              value={city}
+              onChange={handleInputChange}
+            />
+            <div className="flex justify-center mt-4">
+              <Link href={`/${city}`}>
+                <Button type="primary" icon={<SearchOutlined />}>
+                  Get Weather
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Card>
       </div>
     </main>
   );
