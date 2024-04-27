49263e9c2d9be32b29e2767aace7954dfda66864
Lawrence Li <lawrenceli3202@gmail.com>
2024-04-26 22:09:29 -0400

    Style homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,30 +16,28 @@ export default function Home() {
   return (
     <main className="h-screen flex justify-center items-center bg-[#c1defd]">
       <div className="flex justify-center items-center h-full">
-        <Card className="w-full h-7/24 border border-black shadow-lg p-10">
+        <Card className="w-full h-7/24 border border-black shadow-lg p-10 shadow-[#1677ff]">
           <div className="flex items-center justify-center gap-2">
             <h1 className="text-2xl text-center">Find the weather</h1>
             <TiWeatherPartlySunny size={25} />
           </div>
           <Divider />
-          <div>
-            <div className="flex items-center justify-center">
-              <p className="text-lg">Enter a city</p>
-              <FaCity className="ml-2" />
-            </div>
-            <Input
-              className="w-full mt-4"
-              placeholder="City name"
-              value={city}
-              onChange={handleInputChange}
-            />
-            <div className="flex justify-center mt-4">
-              <Link href={`/${city}`}>
-                <Button type="primary" icon={<SearchOutlined />}>
-                  Get Weather
-                </Button>
-              </Link>
-            </div>
+          <div className="flex items-center justify-center">
+            <p className="text-xl">Enter a city</p>
+            <FaCity className="ml-2" />
+          </div>
+          <Input
+            className="w-full mt-4"
+            placeholder="City name"
+            value={city}
+            onChange={handleInputChange}
+          />
+          <div className="flex justify-center mt-4">
+            <Link href={`/${city}`}>
+              <Button type="primary" icon={<SearchOutlined />} >
+                Search
+              </Button>
+            </Link>
           </div>
         </Card>
       </div>
